6350c03ceef89f9be0c0864971db23e6ea91d775
rwnmtwly <120096605+rwnmtwly@users.noreply.github.com>
2023-05-13 00:32:59 +0300

    an arduino code for printing on an LCD using a keypad

---
+++ task#7/LCD_&_Keypad_arduino_project/skldjslfj/skldjslfj.ino
@@ -0,0 +1,40 @@
+
+// include the library code:
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {5, 6, 7, 8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  char key = keypad.getKey();
+  
+  if (key){
+    lcd.print(key);
+  }
+}
